94e03ecb8aa089eb35d24bebf358a6dfa06dcd5a
LiuYeXiu <i_liuyexiu.@cvte.com>
2023-02-28 20:13:24 +0800

    a.c

--- a.c
+++ a.c
@@ -10,10 +10,11 @@ int main()
         sleep(1);
         printf("我是派大星\n");
         printf("我是派大GIAO\n");
-<<<<<<< HEAD
         printf("我是派大星的哥哥\n");
-=======
->>>>>>> master
+        
+        printf("失败并不可怕\n");
+        printf("可怕的是你放弃了你差点就能得到的东西\n");
+        
    }
    
    return 0;
