4553bf7125a7e055b0bf9d680562835bc5c8872b
LiuYeXiu <i_liuyexiu.@cvte.com>
2023-02-28 15:08:33 +0800

    a.c

--- a.c
+++ a.c
@@ -9,7 +9,8 @@ int main()
         printf("Im studing\n");
         sleep(1);
         printf("我是派大星\n");
-        
+        printf("我是派大GIAO\n");
+        printf("我是派大星的哥哥\n");
    }
    
    return 0;
